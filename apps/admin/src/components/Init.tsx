4116c71c0f3180094b99db47abe7f0e53fce4f85
Harkirat Singh <harkiratsingh.tu@gmail.com>
2023-11-27 23:23:41 -0500

    minor changes init

--- apps/admin/src/components/Init.tsx
+++ apps/admin/src/components/Init.tsx
@@ -1,8 +1,6 @@
 import { useEffect } from "react"
-import { getDriversAPI, getOrdersAPI, getPathsAPI, getRentingItemsAPI, getSideItemsAPI } from "../services/ApiService"
+import { getDriversAPI, getOrdersAPI, getPathsAPI } from "../services/ApiService"
 import { useRecoilValue, useSetRecoilState } from "recoil"
-import { rentItemsState } from "../store/atoms/rentItemsAtom"
-import { sideItemsState } from "../store/atoms/sideItemsAtom"
 import { driversState } from "../store/atoms/driversAtom"
 import { ordersAtom, ordersSearchDate } from "../store/atoms/orderAtom"
 import { savedPaths } from "../store/atoms/pathAtom"
@@ -10,8 +8,6 @@ import { OrderType } from "types"
 
 
 const Init = () => {
-    const setRentingItems = useSetRecoilState(rentItemsState)
-    const setSideItems = useSetRecoilState(sideItemsState)
     const setDrivers = useSetRecoilState(driversState)
     const setOrders = useSetRecoilState(ordersAtom)
     const setPaths = useSetRecoilState(savedPaths)
