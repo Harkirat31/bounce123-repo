39a599b64ac9321f827d04057c01782b7b1509f8
harkirat <harkriatsingh.tu@gmail.com>
2023-11-29 17:31:18 -0500

    fixed download csv sample issue

--- apps/admin/prod/index.cjs
+++ apps/admin/prod/index.cjs
@@ -4,8 +4,14 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '../dist/')));
 
+app.get('/src/assets/sample_order_creation_file.csv', (req, res) => {
+  console.log("Hellos")
+  res.header('Content-Type', 'text/csv');
+  res.download(path.join(__dirname, '../', '/src/assets/',"sample_order_creation_file.csv"))
+});
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, '../dist/', 'index.html'));
 });
+
 
 app.listen(3001,()=>console.log("started server at 3001"));
--- apps/admin/src/components/DownloadButtonMainItems.tsx
+++ apps/admin/src/components/DownloadButtonMainItems.tsx
@@ -3,7 +3,7 @@
 function DownloadButtonMainItems() {
     return (<div className="flex items-center mb-8" >
         <div className="bg-blue-700 text-white py-1 px-3 mr-2 rounded hover:bg-blue-600">
-            <a href="src/assets/sample_main_items_creation_file.csv" download="Sample">Download</a>
+            <a href="src/assets/sample_main_items_creation_file.csv" type="text/csv" download="Sample.csv">Download</a>
         </div>
         <p>Sample CSV file</p>
     </div>
