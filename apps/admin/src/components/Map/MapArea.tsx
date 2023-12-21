e35b5c7e26f49ce0ca37164a8e72901b40f5b560
Harkirat Singh <harkiratsingh.tu@gmail.com>
2023-12-20 22:21:09 -0500

    minor refactoring

--- apps/admin/src/components/Map/MapArea.tsx
+++ apps/admin/src/components/Map/MapArea.tsx
@@ -12,7 +12,6 @@ import { userAtom } from "../../store/atoms/userAtom";
 
 /* Left Side of of the map page . Google maps API is integrated in this component to render orders and path data 
    Used React wrapper for google maps API.
-   
 */
 const MapArea = ({ user }: { user: UserType }) => {
     return (
