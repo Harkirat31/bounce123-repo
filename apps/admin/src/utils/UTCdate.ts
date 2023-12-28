790d46f380d01ef6fd40cf0ea458ce46c4c0a02e
Harkirat Singh <harkiratsingh.tu@gmail.com>
2023-12-27 21:17:54 -0500

    refactoring

--- apps/admin/src/utils/UTCdate.ts
+++ apps/admin/src/utils/UTCdate.ts
@@ -1,3 +1,9 @@
+/*
+convert date to UTC at midnight 
+for example date 1/1/2023 time 20:00 hours is utc 2/1/2023 1 am
+so by converting dirct setUTChours(0,0,0,0) just convrt to 2/1/2023 midnight
+so it get day of the date from normal date and add to setUTCDate() , so that date is same as normal date of client side
+*/
 export const convertToUTC = (date: Date) => {
     let newDate = new Date(date)
     newDate.setUTCDate(date.getDate())
