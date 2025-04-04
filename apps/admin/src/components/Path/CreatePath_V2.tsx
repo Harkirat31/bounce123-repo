a7ccc53ad3ede1d2e2945fae73758020b343f256
Harkirat Singh <harkiratsingh.tu@gmail.com>
2025-04-04 19:13:04 -0400

    New Version Create Path Started

---
+++ apps/admin/src/components/Path/CreatePath_V2.tsx
@@ -0,0 +1,21 @@
+
+
+export const CreatePath = () => {
+    return <div className="fixed top-20 right-0 w-[20vh] h-[90vh] bg-red-400">
+ 
+    </div>
+ }
+ 
+ 
+ 
+ const pendingOrders: { id: string }[] = [
+     {
+         id: "1"
+     },
+     {
+         id: "2"
+     },
+     {
+         id: "3"
+     }
+ ]
